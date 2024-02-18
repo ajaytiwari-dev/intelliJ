0d4d3a4374a916738a08db5aa531041e992e9e0a
ajaytiwari-dev <ajaytiwari.dev@gmail.com>
2024-02-19 01:43:34 +0530

    this commit is for keeping the tool on github.

---
+++ IntelliJ/CTMS/src/main/java/com/emids/CTMS/CtmsApplication.java
@@ -0,0 +1,15 @@
+package com.emids.CTMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtmsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CtmsApplication.class, args);
+		System.out.println("Application Started Bro");
+	}
+
+}
---
+++ IntelliJ/CTMS/src/test/java/com/emids/CTMS/CtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emids.CTMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
