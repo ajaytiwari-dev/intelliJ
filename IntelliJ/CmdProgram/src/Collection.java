7e1813f57a8ae93417a1fe0e42e0a014cad977f4
ajaytiwari-dev <ajaytiwari.dev@gmail.com>
2024-02-18 17:20:12 +0530

    adding project

---
+++ IntelliJ/CmdProgram/src/Collection.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+class Collection{
+    public static void main (String[] args) {
+        String s= "(){}[]";
+        boolean status = ValidateStringBracket(s);
+        System.out.println(s);
+        System.out.println(status);
+    }
+
+    private static boolean ValidateStringBracket(String s) {
+        Stack<Character> Stack = new Stack<Character>();
+
+        for(int i=0; i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if (ch==']') {
+                if (Stack.peek()=='[') {
+                    Stack.pop();
+                }else return false;
+            }else if(ch=='}') {
+                if (Stack.peek()=='{') {
+                    Stack.pop();
+                }else return false;
+            }else if (ch==')')
+                if (Stack.peek()=='(') {
+                    Stack.pop();
+                }else return false;
+        }if (Stack.size()==0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
